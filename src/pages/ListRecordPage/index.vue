5547a3298e3b8e3974b164bbdc36432650e4b1bc
Edwin Dijas Chiwona <dijas@Edwins-MacBook-Pro.local>
2021-10-29 18:25:29 +0200

    Template of design

--- src/pages/ListRecordPage/index.vue
+++ src/pages/ListRecordPage/index.vue
@@ -1,41 +1,36 @@
 <template>
-  <div class="container" id="history">
-    <section class="main-content">
-      <audio :src="source" controls class="audio-player" ref="audio"/>
-      <table class="table header">
-        <thead>
-          <th style="width: 10%"></th>
-          <th style="width: 30%">Name</th>
-          <th style="width: 30%">Category</th>
-          <th style="width: 30%"></th>
-        </thead>
-      </table>
-      <table class="table body">
-        <tbody>
-          <tr v-if="data.list.length === 0">
-            <td colspan="4">No record found</td>
-          </tr>
-        </tbody>
-        <tbody v-if="data.list.length > 0">
-          <tr v-for="(item, index) in data.list" v-bind:key="item.item">
-            <td style="width: 10%"><div class="show-index">{{ index + 1 }}</div></td>
-            <td tyle="width: 30%">{{ item.name }}</td>
-            <td tyle="width: 30%">{{ item.category }}</td>
-            <td tyle="width: 30%">
-              <div class="button-circle" v-if="playing !== index" type="button" @click="changeUrl(index, item.url)">
-                <box-icon name="play-circle" type="logo" color="#eb1395" size="md"/>
-              </div>
-              <div class="button-circle" v-if="playing === index" type="button" @click="stopAudio">
-                <box-icon name="stop-circle" type="logo" color="#eb1395" size="md"/>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+  <div :class="$style.container">
+    <section :class="$style.mainContent">
+      <audio :src="source" ref="player" controls :class="$style.player" />
+      <ul :class="$style.heading">
+        <li></li>
+        <li>Name</li>
+        <li>Category</li>
+        <li></li>
+      </ul>
+      <ul :class="$style.table">
+        <li v-if="audios.length === 0" :class="$style.listNotFound">
+          No record found
+        </li>
+        <template v-else>
+          <li v-for="(item, index) in audios" :key="index">
+            <list-item
+              :index="index"
+              :name="item.name"
+              :category="item.category"
+              @play="play"
+              :playing="playing"
+            />
+          </li>
+        </template>
+      </ul>
     </section>
-    <div class="new-record" @click="$router.push({ name: 'RecordingPage' })">
-      <div class="icon-record">
-         <box-icon v-if="!recording" name="microphone" color="#fff" size="md" />
+    <div
+      :class="$style.newRecord"
+      @click="$router.push({ name: 'RecordingPage' })"
+    >
+      <div :class="$style.icoRecord">
+        <ast-ico-mic />
       </div>
       <span>New Recording</span>
     </div>
@@ -43,10 +38,62 @@
 </template>
 
 <script lang="ts">
-import { computed } from 'vue';
-import { Vue, setup } from "vue-class-component";
-import { useStore } from '@/store';
+import { defineAsyncComponent, defineComponent, inject, ref, Ref } from "vue";
+import { AudioSource, AUDIO_PROVIDER_GET } from "store";
+import ListItem from "./ListItem.vue";
+import { useRouter } from "vue-router";
+const AstIcoMic = defineAsyncComponent(
+  () => import("assets/icons/AstIcoMic.vue")
+);
+//Use composition not classes
+export default defineComponent({
+  components: {
+    ListItem,
+    AstIcoMic,
+  },
+  setup() {
+    const audios: Ref<AudioSource[]> | undefined = inject(AUDIO_PROVIDER_GET);
+    const playing = ref(-1);
+    const source: Ref<undefined | string> = ref();
+    const player: Ref<HTMLAudioElement | undefined> = ref();
+    const router = useRouter();
 
+    //Take user to main screen if no audios
+
+    if (!audios || audios.value.length === 0) {
+      router.push("/");
+    }
+
+    const play = (index: number) => {
+      if (!player.value || !audios?.value.length) {
+        return;
+      }
+
+      console.log("about to play");
+
+      if (playing.value === index && !player.value.paused) {
+        player.value.pause();
+        return;
+      } else if (playing.value === index) {
+        player.value.play();
+      }
+
+      source.value = decodeURIComponent(audios.value[index].url);
+      playing.value = index;
+      player.value.play();
+      console.log(source.value);
+    };
+    return {
+      audios,
+      source,
+      play,
+      playing,
+      player,
+    };
+  },
+});
+
+/*
 export default class HistoryPage extends Vue {
   declare $refs: {
     audio: HTMLMediaElement
@@ -73,6 +120,118 @@ export default class HistoryPage extends Vue {
     this.$refs.audio.currentTime = 0;
     this.playing = -1;
   }
-}
+}*/
 </script>
-<style scoped lang="scss" src="./style.scss"></style>
+<style module lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.mainContent {
+  display: grid;
+  row-gap: 0.4em;
+  border-radius: 1em;
+  background: #fff;
+  box-shadow: -0.2em -0.2em 0.4em -0.1em #f3f1f1,
+    0.2em 0.2em 0.4em -0.1em #dddddd;
+  overflow: hidden;
+  max-height: calc(100vh - 4em);
+}
+
+.player {
+  display: none;
+}
+
+.heading,
+.table > li > ul {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5em;
+}
+
+.heading {
+  padding: 1em 0.5em;
+  border-bottom: 0.25em solid #eee;
+  font-weight: 600;
+  color: #000;
+}
+
+.heading li,
+.table > li > ul > li {
+  width: calc((100% - 6em) / 2);
+  padding: 0 0.5em;
+  text-align: left;
+  line-height: 1.8;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  &:first-child,
+  .last-child {
+    width: 3em;
+    text-align: right;
+  }
+  &::first-letter {
+    text-transform: capitalize;
+    font-size: 1.1em;
+  }
+}
+
+.table > li {
+  width: 100%;
+  text-align: center;
+}
+
+.table > li > ul {
+  border-bottom: 1px solid #f5f5f5;
+  color: #444;
+}
+
+.table > li:last-child > ul {
+  border: 0;
+}
+
+.table {
+  width: 45em;
+  max-width: 100%;
+  background: $color-white;
+  max-height: calc(100vh - 10em);
+  overflow: auto;
+  margin-bottom: 0.5em;
+}
+
+.icoRecord {
+  width: 4.375em;
+  height: 4.375em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  padding: 1.4em;
+  fill: #fff;
+  background: $color-pink;
+  box-shadow: 0 0 0 0.25em #d0d0d0;
+  margin-bottom: 0.5em;
+}
+
+.newRecord {
+  position: fixed;
+  right: 1rem;
+  bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25em;
+  .icon-record {
+    cursor: pointer;
+    .voice-button {
+      width: 1.875em;
+      height: 1.875em;
+      color: $color-white;
+    }
+  }
+}
+</style>
--- src/pages/RecordingInformationPage/index.vue
+++ src/pages/RecordingInformationPage/index.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="container" id="recording-information-page">
-    <section class="main-content">
-      <form class="form" @submit="onSubmit">
-        <div>
-          <box-icon name="microphone" color="#808080" size="md" />
+  <div :class="$style.container">
+    <section :class="$style.mainContent">
+      <form :class="$style.form" @submit.prevent="save">
+        <div :class="$style.fig">
+          <ast-ico-mic />
         </div>
-        <h2 class="title">Recording Information</h2>
-        <input type="text" v-model="name" class="form-control" placeholder="Name of Recording"/>
-        <input type="text" v-model="category" class="form-control" placeholder="Audio Category"/>
+        <h2 :class="$style.title">Recording Information</h2>
+        <input
+          type="text"
+          v-model="name"
+          :class="$style.formControl"
+          placeholder="Name of Recording"
+        />
+        <input
+          type="text"
+          v-model="category"
+          :class="$style.formControl"
+          placeholder="Audio Category"
+        />
         <div>
-          <button type="submit" :disabled="!name || !category" class="button">Save Recording</button>
+          <button
+            type="submit"
+            :disabled="!name || !category"
+            :class="$style.button"
+          >
+            Save Recording
+          </button>
         </div>
       </form>
     </section>
@@ -17,30 +33,104 @@
 </template>
 
 <script lang="ts">
-import { Vue, setup } from "vue-class-component";
-import { useStore } from '@/store';
-import 'boxicons';
-export default class RecordingInformationPage extends Vue {
-  name: string = '';
-  category: string = '';
-  id: number = 1;
-  action: any = setup(() => {
-    const store = useStore();
-    return { updateUrl: store.updateUrl };
-  });
+import { defineAsyncComponent, defineComponent } from "@vue/runtime-core";
+import { AUDIO_PROVIDER_SAVE, AUDIO_PROVIDER_SAVE_FUNC } from "store";
+import { inject, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
-  mounted () {
-    const { id: urlId } = this.$route.params;
-    if (!Number.isInteger(+urlId) || +urlId < 1) {
-      return alert('Invalid id')
-    }
-    this.id = +urlId;
-  }
-  async onSubmit (e: Event) {
-    e.preventDefault();
-    this.action.updateUrl(this.id, this.name, this.category);
-    this.$router.push({ name: 'List' })
-  }
-}
+const AstIcoMic = defineAsyncComponent(
+  () => import("assets/icons/AstIcoMic.vue")
+);
+
+export default defineComponent({
+  components: {
+    AstIcoMic,
+  },
+  setup() {
+    const name = ref("");
+    const category = ref("");
+    const route = useRoute();
+    const router = useRouter();
+    const { id } = route.params;
+    const saveFile: AUDIO_PROVIDER_SAVE_FUNC | undefined =
+      inject(AUDIO_PROVIDER_SAVE);
+    const save = () => {
+      if (!saveFile || !name.value || !category.value) {
+        return;
+      }
+      saveFile(id as string, name.value, category.value);
+      router.push("/list");
+    };
+
+    console.log(id);
+    return {
+      name,
+      category,
+      save,
+    };
+  },
+});
 </script>
-<style scoped lang="scss" src="./style.scss"></style>
+<style module lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.mainContent {
+  width: 30em;
+  padding: 3.125em;
+  background: rgba(#e8e8e8, 0.9);
+  border-radius: 1em;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 3px;
+  background-image: linear-gradient(-19deg, #e9e8e8, transparent);
+}
+
+.fig {
+  width: 1.5em;
+  height: 1.5em;
+  margin: 0 auto;
+  fill: #575756;
+}
+
+.form {
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  gap: 1.5rem;
+}
+
+.formControl {
+  padding: 1.25em;
+  outline: none;
+  border: none;
+  border-radius: 1em;
+  //box-shadow: -0.2em -0.2em 0.4em -0.1em #fff, 0.2em 0.2em 0.4em -0.1em #b5b5b5;
+  border: 1px solid #e5e5e5;
+}
+
+.button {
+  outline: none;
+  border: none;
+  border-radius: 4em;
+  width: 100%;
+  cursor: pointer;
+  font-size: 1em;
+  font-weight: 600;
+  background: $color-pink;
+  color: #fff;
+  height: 4em;
+  box-shadow: -0.2em -0.2em 0.4em -0.1em #fff, 0.2em 0.2em 0.4em -0.1em #b5b5b5;
+}
+
+.title {
+  margin: 0;
+  font-size: 1.4em;
+  font-weight: 700;
+  color: #000;
+}
+</style>
